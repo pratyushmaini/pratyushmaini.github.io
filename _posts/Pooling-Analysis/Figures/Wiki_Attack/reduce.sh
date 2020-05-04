b2b3142aa4d0f51696105d70469c4d6b298524c4
pratyush911 <pratyush.maini@gmail.com>
2020-05-04 17:43:51 +0530

    PNG

--- _posts/Pooling-Analysis/Figures/Wiki_Attack/reduce.sh
+++ _posts/Pooling-Analysis/Figures/Wiki_Attack/reduce.sh
@@ -4,9 +4,11 @@ echo $filename
 done
 
 for filename in reduced-*.pdf; do
-echo $filename
-new_file="$(echo $filename | cut -d'-' -f2)"
-echo $new_file
-mv $filename $new_file
+	echo $filename
+	new_file="$(echo $filename | cut -d'-' -f2)"
+	echo $new_file
+	mv $filename $new_file
+	sips -s format png --out "${new_file%%.*}.png" "$new_file"
+	rm $new_file
 done
 
